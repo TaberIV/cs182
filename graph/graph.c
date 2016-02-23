48971932e44ff722f7ee4a5cba868c894ff1aa80
Taber McFarlin <Taber McFarlin>
2016-02-23 18:08:08 -0500

    Stiff

--- graph/graph.c
+++ graph/graph.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <math.h>
 
 #include "graph.h"
 #include "SIdict.c"
@@ -17,18 +18,21 @@ struct graph {
 Graph makeGraph(int n, int rep) {
   Graph g = (Graph) malloc(sizeof(struct graph));
   g->n = n;
+  int i, j;
   
   if (rep == MATRIX) {
-    g->matrix = malloc(sizeof(float[n][n]));
     g->rep = MATRIX;
+    g->matrix = malloc(sizeof(float[n][n]));
+    
+    for(i = 0; i < n; i++)
+      for(j = 0; j < n; j++)
+	g->matrix[i][j] = INFINITY;
   }
   else {
     g->list = malloc(sizeof(SIdict[n]));
 
-    int i;
-    for(i = 0; i < n; i++) {
+    for(i = 0; i < n; i++)
       g->list[i] = makeSIdict();
-    }
     
     g->rep = LIST;
   }
@@ -37,10 +41,29 @@ Graph makeGraph(int n, int rep) {
 }
 
 Graph cloneGraph(Graph G, int rep) {
-  Graph g = malloc(sizeof(struct graph));
+  Graph g = makeGraph(G->n, rep);
+  int i, j;  
   
-  if(G->rep == rep) {
-    g->matrix = NULL;
+  if (G->rep == rep) {
+    if (rep == MATRIX) {
+      for (i = 0; i < g->n; i++)
+	for (j = 0; j < g->n; j++)
+	  g->matrix[i][j] = G->matrix[i][j];
+    }
+    else {
+      //Copy linked list
+      return NULL;
+    }
+  }
+  else {
+    if (rep == MATRIX) {
+      //Convert Linked list to matrix
+      return NULL;
+    }
+    else {
+      //Convert Matrix to linked list
+      return NULL;
+    }
   }
 
   return g;
@@ -57,7 +80,7 @@ int numVerts(Graph G) {
 int addEdge(Graph G, int source, int target, float w) {
   if (source > G->n && target > G->n) {
     if (G->rep == MATRIX) {
-      if (G->matrix[source][target] == 0)
+      if (G->matrix[source][target] == INFINITY)
 	G->matrix[source][target] = w;
       else
 	return ERROR;
@@ -74,8 +97,8 @@ int addEdge(Graph G, int source, int target, float w) {
 int delEdge(Graph G, int source, int target) {
   if (source > G->n && target > G->n) {
     if (G->rep == MATRIX) {
-      if (G->matrix[source][target] != 0)
-	G->matrix[source][target] = 0;
+      if (G->matrix[source][target] != INFINITY)
+	G->matrix[source][target] = INFINITY;
       else
 	return ERROR;
     }
@@ -100,3 +123,18 @@ float edge(Graph G, int source, int target) {
     return -1;
 }
 
+int* successors(Graph G, int source) {
+  int* targets = malloc(sizeof(int[G->n + 1]));
+
+  int i, numVerts = 0;
+  for (i = 0; i < G->n; i++) {
+    if (G->matrix[i] != INFINITY) {
+      targets[numVerts] = i;
+      numVerts++;
+    }
+  }
+}
+
+int* predecessors(Graph G int target) {
+
+}

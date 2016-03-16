2e8779ebdc2df9f13a6f8d5d15343c27f98a0f62
Taber McFarlin <tabermcfarlin@gmail.com>
2016-03-16 15:55:58 -0400

    Okay let's do quicksort ONE MORE TIME and get it right. Use the book. Okay OUAT! :) :D

--- sorting/sortingTest.c
+++ sorting/sortingTest.c
@@ -2,13 +2,8 @@
 
 int main() {
   char* arr[5] = {"Taber", "Brendan", "Gavin", "Nick", "Cheyenne"};
-  /*
-  strcpy(arr[0], "Taber");
-  strcpy(arr[1], "Brendan");
-  strcpy(arr[2], "Gavin");
-  strcpy(arr[3], "Nick");
-  strcpy(arr[4], "Cheyenne");
-  */
+  char* arr1[5] = {"Taber", "Brendan", "Gavin", "Nick", "Cheyenne"};
+  
   printf("Unsorted: \n");
   for (int i = 0; i < 5; i++)
     printf("%s \n", arr[i]);
@@ -17,4 +12,9 @@ int main() {
   insertionsort(arr, 0, 5);
   for (int i = 0; i < 5; i++)
     printf("%s \n", arr[i]);
+  
+  printf("\nQuicksort: \n");
+  quicksort(arr1, 0, 5);
+  for (int i = 0; i < 5; i++)
+    printf("%s \n", arr1[i]);
 }

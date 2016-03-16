57fa821ed0ef8177b0eb986fbf6ff591c96d8260
Taber McFarlin <tabermcfarlin@gmail.com>
2016-03-16 15:26:07 -0400

    Fixed string problems

--- sorting/sorting.c
+++ sorting/sorting.c
@@ -2,37 +2,42 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
+#include "sorting.h"
 
-void swap(int* arr, int a, int b) {
-  int temp = arr[a];
+void swap(char* arr[], int a, int b) {
+  char* temp = arr[a];
   arr[a] = arr[b];
   arr[b] = temp;
 }
 
-void insertionsort(int* arr, int l, int r) {
+void insertionsort(char* arr[], int l, int r) {
   for (int i = l + 1; i < r; i++)
-    for (int j = i; j >= 0 && arr[j - 1] > arr[j]; j--)
+    for (int j = i; j > 0 && strcmp(arr[j - 1], arr[j]) > 0; j--)
       swap(arr, j - 1, j);
 }
 
-void quicksort(int* arr, int i, int r) {
+void quicksort(char* arr[], int i, int r) {
+  
+}
+
+/*void quicksort(char* arr[], int i, int r) {
   for (int i = 0; i < 5; i++)
-    printf("%d \n", arr[i]);
+    printf("%s \n", arr[i]);
   
   int start = i;
   int end = r;
-  int pivot = arr[i];
+  char* pivot = arr[i];
   if (r - i > 1) {
     while (i < r-1) {
-      while (i < r && arr[i] <= pivot) {
+      while (i < r && strcmp(arr[i], pivot) <= 0) {
 	printf("Increment i\n");
 	i++;
       }
-      while (r-1 > i && arr[r - 1] > pivot) {
+      while (r-1 > i && strcmp(arr[r - 1], pivot) > 0) {
 	printf("Decrement r\n");
 	r--;
       }
-      printf("Swap indecies %d and %d (values %d and %d)\n", i, r - 1, arr[i], arr[r - 1]);
+      printf("Swap indecies %d and %d (values %s and %s)\n", i, r - 1, arr[i], arr[r - 1]);
       swap(arr, i, r - 1);
     }
     swap(arr, start, i - 1);
@@ -41,12 +46,12 @@ void quicksort(int* arr, int i, int r) {
     printf("Sort from %d to %d\n", r, end);
     quicksort(arr, r, end);
   }
+  }*/
+
+void quicksortPlus(char* arr[], int i, int r) {
+
 }
 
-void quicksortPlus(int* arr, int i, int r) {
-
-}
-
-void compareSorts(int* arr, int len) {
+void compareSorts(char* arr[], int len) {
 
 }

3597680c14fbbeb63a137b98f3581ffcb955ae9e
Taber McFarlin <tabermcfarlin@gmail.com>
2016-03-30 02:31:13 -0400

    Non-Balance addOrUpdate() finished

--- BSTbal/SVdict.c
+++ BSTbal/SVdict.c
@@ -38,42 +38,63 @@ int hasKey(SVdict d, char* key) {
   return 0;
 }
 
-int addOrUpdate(SVdict d, char* key, void* val) {  
-  while (d != NULL && d->key != NULL) {    
+int addOrUpdate(SVdict d, char* key, void* val) { //ADD BALANCING
+  SVdict prev = NULL;
+  
+  while (d != NULL && d->val != NULL) {
     if (strcmp(d->key, key) == 0) {
-      printf("1");
       d->val = val;
       return 1;
     }
-    else if (strcmp(d->key, key) < 0) {
-      printf("Go right\n");
-      if (d->right == NULL) {
-	d->right = makeSVdict();
-	d->right->key = key;
-	d->right->val = val;
-
-	return 0;
-      }
-      else
-	d = d->right;
-    }
     else {
-      printf("Go left\n");
-      if (d->left == NULL) {
-	d->left = makeSVdict();
-	d->left->key = key;
-	d->left->val = val;
-	
-	return 0;
-      }
+      prev = d;
+      
+      if (strcmp(d->key, key) < 0)
+	d = d->right;
+      else
+	d = d->left;
     }
   }
+  //either d is null, or we were dealing with an empty tree, in which case prev = NULL
+  if (prev != NULL) {
+    if (strcmp(prev->key, key) < 0) {
+      prev->right = makeSVdict();
+      d = prev->right;
+    }
+    else {
+      prev->left = makeSVdict();
+      d = prev->left;
+    }
+  }
+  d->val = val;
+  d->key = (char*) malloc(sizeof(key));
+  strcpy(d->key, key);
+  
+  return 0;
+}
+
+void* lookup(SVdict d, char* key) {
+  while (d != NULL && d->val != NULL) {
+    if (strcmp(d->key, key) == 0)
+      return d->val;
+    else if (strcmp(d->key, key) < 0)
+      d = d->right;
+    else
+      d = d->left;
+  }
+  return NULL;
+}
+
+int remKey(SVdict d, char* key) {
+  
 }
 
 int main() {
   SVdict dict = makeSVdict();
-  addOrUpdate(dict, "ASS", (int*) 3);
-  addOrUpdate(dict, "BASS", (int*) 4);
-  assert((strcmp("BASS", dict->right->key) == 0));
-  addOrUpdate(dict, "APPLE", (int*) 7);
+  addOrUpdate(dict, "GOOD", (int*) 1);
+  addOrUpdate(dict, "BAD", (int*) 3);
+  addOrUpdate(dict, "ASS", (int*) 7);
+  addOrUpdate(dict, "UGLY", (int*) 32);
+
+  //printf("%d\n", lookup(dict, "UGLY"));
 }
